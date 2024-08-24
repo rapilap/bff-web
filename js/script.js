ed9f48a2b806a19d3f2e10b33fa2fe0fc33757c8
apilap <raffyabdillah03@gmail.com>
2024-08-24 16:20:14 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#ham-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
