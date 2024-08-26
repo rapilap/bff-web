0ba01c386f02f4cc91b56f9ad6dc51f3be44bf4a
Raffy <raffyabdillah03@gmail.com>
2024-08-26 18:03:41 +0700

    add content

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#ham-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+// Outside Click
+const ham = document.querySelector("#ham-menu");
+
+document.addEventListener("click", function (e) {
+  if (!ham.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
