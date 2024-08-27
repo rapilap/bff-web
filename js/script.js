2beb46b1c188161bc9b0f1cd5b49f382db39e827
Raffy <raffyabdillah03@gmail.com>
2024-08-27 20:25:51 +0700

    add search function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
+// Hamburger Menu Start
 document.querySelector("#ham-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
@@ -11,5 +14,21 @@ const ham = document.querySelector("#ham-menu");
 document.addEventListener("click", function (e) {
   if (!ham.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+});
+// Hamburger Menu End
+
+// Search Start
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Outside Click
+const unSearch = document.querySelector("#search-button");
+document.addEventListener("click", function (e) {
+  if (!unSearch.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
