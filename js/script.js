f7ec8ea029a85914226d913fa6e473918dd1d058
Raffy <raffyabdillah03@gmail.com>
2024-08-29 22:59:33 +0700

    add cart and detail

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const navbarNav = document.querySelector(".navbar-nav");
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
+const shopCart = document.querySelector(".shopping-cart");
 
 // Hamburger Menu Start
 document.querySelector("#ham-menu").onclick = () => {
@@ -32,3 +33,41 @@ document.addEventListener("click", function (e) {
     searchForm.classList.remove("active");
   }
 });
+// Search End
+
+// Shopping Cart Start
+document.querySelector("#shopping-cart").onclick = () => {
+  shopCart.classList.toggle("active");
+};
+
+// Outside Click
+const cart = document.querySelector("#shopping-cart");
+
+document.addEventListener("click", function (e) {
+  if (!cart.contains(e.target) && !shopCart.contains(e.target)) {
+    shopCart.classList.remove("active");
+  }
+});
+// Hamburger Menu End
+
+// Modal Box Start
+const itemDetailModel = document.querySelector("#item-detail-modal");
+const itemDetailButton = document.querySelectorAll(".item-detail-button");
+
+itemDetailButton.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModel.style.display = " flex";
+    e.preventDefault();
+  };
+});
+
+document.querySelector(".modal .close-container").onclick = (e) => {
+  itemDetailModel.style.display = "none";
+  e.preventDefault();
+};
+
+window.onclick = (e) => {
+  if (e.target === itemDetailModel) {
+    itemDetailModel.style.display = "none";
+  }
+};
